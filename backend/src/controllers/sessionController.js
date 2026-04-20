558f9ec001e4177c39485b677bb62af79460fee8
divyanshu <ds9158044@gmail.com>
2026-04-21 00:58:12 +0530

    fix

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,3 +1,9 @@
+export async function createSession(req, res) {}
+export async function getActiveSessions(req, res) {}
+export async function getMyRecentSessions(req, res) {}
+export async function getSessionById(req, res) {}
+export async function joinSession(req, res) {}
+export async function endSession(req, res) {}
 export async function joinSession(req, res) {
   try {
     const { id } = req.params;
